2ef96d7483fcd9a710c6699e8aa5ebeb8de49545
zamharrad <mhmdzamhar@gamil.com>
2020-03-31 11:28:33 +0530

    add chamge in cardlist components and cardlist css

---
+++ src/components/cardList/CardList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./CardList.css";
+
+const CardList = props => {
+  console.log(props);
+  return (
+    <div className="card-list">
+      {props.monsters.map(monsters => (
+        <h1 key={monsters.id}> {monsters.name}</h1>
+      ))}
+    </div>
+  );
+};
+
+export default CardList;
