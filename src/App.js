731f75a2d145d2bca432725069cf81f504589734
zamharrad <mhmdzamhar@gamil.com>
2020-04-03 08:51:51 +0530

    add styles to searcbox

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ class App extends Component {
 
   render() {
     const { monsters, searchField } = this.state;
+
     const filteredMonsters = monsters.filter(monster =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
